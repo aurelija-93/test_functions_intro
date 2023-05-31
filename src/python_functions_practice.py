4504a920e4809112c98d26a705c937dca10a12fe
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-05-31 19:49:21 +0100

    all tests passed

---
+++ src/python_functions_practice.py
@@ -0,0 +1,66 @@
+def return_10():
+    return 10
+
+def add(first_num, second_num):
+    return first_num + second_num
+
+def subtract(first_num, second_num):
+    return first_num - second_num
+
+def multiply(first_num, second_num):
+    return first_num * second_num
+
+def divide(first_num, second_num):
+    return first_num / second_num
+
+def length_of_string(test_string):
+    return len(test_string)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(string_1, string_2):
+    return int(string_1) + int(string_2)
+
+def number_to_full_month_name(number):
+    months = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    }
+    return months[number]
+
+def number_to_short_month_name(number):
+    months = {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec"
+    }
+    return months[number]
+
+def volume_of_cube(length):
+    return length ** 3
+
+def reverse_string(string):
+    return string[::-1]
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) / 1.8
